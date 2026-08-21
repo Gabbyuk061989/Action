14afc4a25818c1036cad217c691d7f13e72cc714
Gabbyuk061989 <gabbyuk1989@gmail.com>
2026-08-21 13:18:20 +0000

    change

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+terraform {
+  # Terraform version at the time of writing this post
+  required_version = "> 0.12.24"
 
+  backend "s3" {
+    bucket = "cloudquickpocsbackendtf123"
+    key    = "quickcloudpocsbackend123.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
